c3a6ff9cec718c59a3d99064254d3de2f7defa03
AbigailMatthews <abigail.m04@hotmail.com>
2020-11-24 00:34:55 +1100

    cds upload

--- CDS-Assignment/assets/js/main.js
+++ CDS-Assignment/assets/js/main.js
@@ -10,6 +10,18 @@ $( document ).ready(function() {
     $('.new-message4').addClass('hide');    
     $('.new-message5').addClass('hide');    
     $('.new-message6').addClass('hide');    
+
+    $('.btn-interface').addClass('hide'); 
+    $('.btn-address').addClass('hide');    
+    $('.btn-city').addClass('hide');    
+    $('.btn-cloud').addClass('hide'); 
+    $('.btn-earth').addClass('hide');     
+    $('.interface-text').addClass('hide');  
+    $('.address-text').addClass('hide');          
+    $('.city-text').addClass('hide');  
+    $('.cloud-text').addClass('hide'); 
+    $('.earth-text').addClass('hide');       
+
   } );  
 
 
@@ -34,6 +46,35 @@ $( document ).ready(function() {
   }); 
 
 
+
+  $('.btn-user').click(function(){  
+    $('.btn-interface').removeClass('hide');   
+    $('.interface-text').removeClass('hide');                
+  }); 
+
+  $('.btn-interface').click(function(){
+    $('.btn-address').removeClass('hide');   
+    $('.address-text').removeClass('hide');         
+  });   
+
+  $('.btn-address').click(function(){
+    $('.btn-city').removeClass('hide');   
+    $('.city-text').removeClass('hide');         
+  });   
+
+  $('.btn-city').click(function(){
+    $('.btn-cloud').removeClass('hide');   
+    $('.cloud-text').removeClass('hide');         
+  }); 
+
+  $('.btn-cloud').click(function(){
+    $('.btn-earth').removeClass('hide');   
+    $('.earth-text').removeClass('hide');         
+  });       
+
 });
+
+
+
 
 
