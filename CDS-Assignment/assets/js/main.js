f7520b4bbf95f0a3e02c0ebdb27cb6a34b2cd386
AbigailMatthews <abigail.m04@hotmail.com>
2020-11-24 10:36:23 +1100

    assignement changes

--- CDS-Assignment/assets/js/main.js
+++ CDS-Assignment/assets/js/main.js
@@ -74,6 +74,36 @@ $( document ).ready(function() {
 
 });
 
+$( document ).ready(function() {
+$( ".title-1" ).click(function() {
+  $( ".toggle-1" ).slideToggle( "slow" );
+});
+
+$( ".title-2" ).click(function() {
+  $( ".toggle-2" ).slideToggle( "slow" );
+});
+
+$( ".title-3" ).click(function() {
+  $( ".toggle-3" ).slideToggle( "slow" );
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
 
 
 
